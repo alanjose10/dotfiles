f6e7b60e9c08798009398d2b776fe85da4b2d714
Alan Jose <alanjose10@gmail.com>
2025-11-26 16:51:52 +0000

    adding more plugins

--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -162,6 +162,21 @@ return {
 					vim.lsp.buf.format({ async = false })
 				end,
 			})
+
+			-- Popup diagnostic
+			vim.o.updatetime = 250
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					vim.diagnostic.open_float(nil, { focus = false })
+				end,
+			})
 		end,
 	},
 	{
--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -26,5 +26,9 @@ return {
 				{ name = "path" },
 			}),
 		})
+
+		-- Integrating autopairs with nvim-cmp
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 	end,
 }
