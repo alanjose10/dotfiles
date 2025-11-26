49999282a9e41d40d1d1459a73e09b98d128f052
Alan Jose <alanjose10@gmail.com>
2025-11-26 00:53:30 +0000

    building ultimate nvim

---
+++ home/.config/nvim/lua/plugins/completions.lua
@@ -0,0 +1,64 @@
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+						-- For `mini.snippets` users:
+						-- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+						-- insert({ body = args.body }) -- Insert at cursor
+						-- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+						-- require("cmp.config").set_onetime({ sources = {} })
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered({
+						border = "rounded",
+					}),
+					documentation = cmp.config.window.bordered({
+						border = "rounded",
+					}),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
+}
--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -21,18 +21,22 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+		},
 		config = function()
 			-- Capabilities for nvim-cmp
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			-- Load lspconfig so builtin server configs are available
-			require("lspconfig")
 
 			-----------------------------------------------------------------------
 			-- Global defaults for ALL LSP servers
 			-----------------------------------------------------------------------
 			vim.lsp.config("*", {
 				capabilities = capabilities,
+				root_markers = {
+					".git",
+				},
 			})
 
 			vim.lsp.config("lua_ls", {
@@ -84,6 +88,11 @@ return {
 			-- gopls: disable formatting, use gofumpt instead
 			-----------------------------------------------------------------------
 			vim.lsp.config("gopls", {
+				settings = {
+					gopls = {
+						gofumpt = true,
+					},
+				},
 				on_attach = function(client)
 					-- Turn OFF gopls formatting so gofumpt (none-ls) is used instead
 					client.server_capabilities.documentFormattingProvider = false
@@ -129,6 +138,30 @@ return {
 					end
 				end,
 			})
+
+			-- Auto format go imports on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					local params = vim.lsp.util.make_range_params()
+					params.context = { only = { "source.organizeImports" } }
+					-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+					-- machine and codebase, you may want longer. Add an additional
+					-- argument after params if you find that you have to write the file
+					-- twice for changes to be saved.
+					-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+					local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+					for cid, res in pairs(result or {}) do
+						for _, r in pairs(res.result or {}) do
+							if r.edit then
+								local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+								vim.lsp.util.apply_workspace_edit(r.edit, enc)
+							end
+						end
+					end
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
 		end,
 	},
 	{
@@ -144,8 +177,14 @@ return {
 					-- Golang
 					null_ls.builtins.formatting.gofumpt,
 					null_ls.builtins.diagnostics.golangci_lint,
+
+					-- Python
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
 				},
 			})
+
+			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format" })
 		end,
 	},
 }
