db1c65bd1a97cfc90a4dc9a1471e4433e27781b0
Alan Jose <alanjose10@gmail.com>
2025-11-26 17:51:17 +0000

    add keymap for git files in telescope

--- home/.config/nvim/lua/plugins/nvim-telescope.lua
+++ home/.config/nvim/lua/plugins/nvim-telescope.lua
@@ -1,27 +1,28 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "v0.1.9",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader><BS>', builtin.oldfiles, { desc = 'Telescope old files' })
-      vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Telescope buffers" })
-    end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "v0.1.9",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files tracked by git" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader><BS>", builtin.oldfiles, { desc = "Telescope old files" })
+			vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Telescope buffers" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
