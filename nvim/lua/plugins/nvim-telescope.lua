b2eb357eb7bc3ce60919dce51edb3c4558e49445
Alan <alan@Alans-Mac-mini.local>
2025-12-02 22:56:11 +0000

    telescope

--- nvim/lua/plugins/nvim-telescope.lua
+++ nvim/lua/plugins/nvim-telescope.lua
@@ -3,14 +3,54 @@ return {
 		"nvim-telescope/telescope.nvim",
 		cmd = { "Telescope", "TelescopeFindFiles" },
 		tag = "v0.1.9",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+		},
 		config = function()
+			local telescope = require("telescope")
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+
+			telescope.setup({
+				defaults = {
+					layout_strategy = "horizontal",
+					layout_config = { prompt_position = "top", preview_width = 0.55 },
+					sorting_strategy = "ascending",
+					path_display = { "smart" },
+					file_ignore_patterns = {
+						".git/",
+						"bazel%-bin",
+						"bazel%-out",
+						"bazel%-testlogs",
+						"plz%-out",
+						"node_modules",
+						"%.cache",
+					},
+					dynamic_preview_title = true,
+				},
+			})
+
+			pcall(telescope.load_extension, "fzf")
+
+			vim.keymap.set("n", "<leader>ff", function()
+				builtin.find_files({ hidden = true, follow = true })
+			end, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files tracked by git" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader><BS>", builtin.oldfiles, { desc = "Telescope old files" })
+			vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Grep string under cursor" })
+			vim.keymap.set("n", "<leader><BS>", function()
+				builtin.oldfiles({ only_cwd = true })
+			end, { desc = "Recent files (cwd)" })
 			vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Workspace diagnostics" })
+			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume last picker" })
+			vim.keymap.set("n", "<leader>fsb", builtin.current_buffer_fuzzy_find, { desc = "Search in buffer" })
 		end,
 	},
 	{
