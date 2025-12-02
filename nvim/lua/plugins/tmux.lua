3a0806e6671c201e6840c0116636538fd2362677
Alan <alan@Alans-Mac-mini.local>
2025-12-02 22:27:51 +0000

    tmux nvim nv

---
+++ nvim/lua/plugins/tmux.lua
@@ -0,0 +1,12 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+	init = function()
+		-- Alt+Arrow navigation (Colemak-friendly and avoids macOS Ctrl+Arrow shortcuts)
+		vim.g.tmux_navigator_no_mappings = 1
+		vim.keymap.set("n", "<A-Left>", ":TmuxNavigateLeft<CR>", { silent = true })
+		vim.keymap.set("n", "<A-Right>", ":TmuxNavigateRight<CR>", { silent = true })
+		vim.keymap.set("n", "<A-Up>", ":TmuxNavigateUp<CR>", { silent = true })
+		vim.keymap.set("n", "<A-Down>", ":TmuxNavigateDown<CR>", { silent = true })
+	end,
+}
