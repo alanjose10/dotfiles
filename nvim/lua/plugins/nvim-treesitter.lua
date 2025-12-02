6b728fb3ffc5393f637263740e2f1cf90a368f86
Alan <alan@Alans-Mac-mini.local>
2025-12-02 23:02:01 +0000

    treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -6,12 +6,39 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
+			ensure_installed = {
+				"bash",
+				"dockerfile",
+				"go",
+				"gomod",
+				"gowork",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
 			auto_install = true,
 			highlight = {
-				enabled = true,
+				enable = true,
 			},
-			intend = {
-				enabled = true,
+			indent = {
+				enable = true,
+				disable = { "yaml" },
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					node_decremental = "grm",
+				},
 			},
 		})
 	end,
