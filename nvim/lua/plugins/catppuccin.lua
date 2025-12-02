10045dc9e4f213275c9549db4a51757b854d749e
Alan <alan@Alans-Mac-mini.local>
2025-12-02 22:19:57 +0000

    catppuccin changes

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,21 @@
-return { 
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+			term_colors = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				mason = true,
+				neotree = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
