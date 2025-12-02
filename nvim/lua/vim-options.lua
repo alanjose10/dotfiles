408d020753a51805c1e39b13d32b5a180d389bc6
Alan <alan@Alans-Mac-mini.local>
2025-12-02 22:14:30 +0000

    nvim options updates

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,25 +1,66 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
+vim.opt.background = "light"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
-vim.g.mapleader = " "
-vim.g.background = "light"
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.grepprg = "rg --vimgrep --hidden"
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.updatetime = 300
+vim.opt.wrap = false
 
--- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- Filetype-specific indentation so Go/Python/YAML feel natural
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go" },
+	callback = function()
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python" },
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "yaml", "yml", "helm", "docker-compose" },
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "bzl", "starlark" },
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
