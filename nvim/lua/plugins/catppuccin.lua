8152749d9a4d3eebb673a2a7f7ca749759d2f44b
Alan Jose <alanjose10@gmail.com>
2025-12-13 01:26:59 +0530

    different catppuccin flavor based on work/home

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,19 @@
+-- Use different flavour for home and work
+local function get_catppuccin_flavour()
+	local env = vim.env.DOTFILE_ENV
+	if env == "home" then
+		return "frappe"
+	end
+	return "macchiato"
+end
+
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "mocha",
+			flavour = get_catppuccin_flavour(),
 			term_colors = true,
 			integrations = {
 				cmp = true,
