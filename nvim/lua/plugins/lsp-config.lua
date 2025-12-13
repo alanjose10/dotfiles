b55a15a005108c4afa64ef4ddbac6e25505b9ee7
Alan Jose <alanjose10@gmail.com>
2025-12-13 14:23:02 +0530

    lsp cleanup

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,8 +22,11 @@ return {
 			"saghen/blink.cmp",
 			{
 				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
 				opts = {
 					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
 						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 					},
 				},
