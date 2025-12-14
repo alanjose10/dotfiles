5fe0e52620f63707643a289445df2d6310a050bd
Alan Jose <alanjose10@gmail.com>
2025-12-15 03:38:03 +0530

    update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,23 +1,24 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin",
-        component_separators = { left = "│", right = "│" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_x = { "encoding", "filetype" },
-      },
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				component_separators = { left = "│", right = "│" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = { "encoding", "filetype" },
+			},
+			always_show_tabline = true,
+		})
+	end,
 }
