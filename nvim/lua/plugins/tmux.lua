edbcfac1b3e56668b5d2b430092583f4bbddf73a
Alan Jose <alanjose10@gmail.com>
2025-12-16 01:49:11 +0530

    cleanup tmux plugin keymaps

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -8,24 +8,12 @@ return {
 			multiplexer_integration = "tmux",
 		})
 
-		-- Movement (works in Vim splits and tmux panes)
 		local maps = {
-			-- Alt/Meta arrows
-			{ "<A-Left>", ss.move_cursor_left, "Pane left" },
-			{ "<A-Right>", ss.move_cursor_right, "Pane right" },
-			{ "<A-Up>", ss.move_cursor_up, "Pane up" },
-			{ "<A-Down>", ss.move_cursor_down, "Pane down" },
-			{ "<M-Left>", ss.move_cursor_left, "Pane left" },
-			{ "<M-Right>", ss.move_cursor_right, "Pane right" },
-			{ "<M-Up>", ss.move_cursor_up, "Pane up" },
-			{ "<M-Down>", ss.move_cursor_down, "Pane down" },
-			-- Leader + arrows as a terminal-agnostic fallback
-			{ "<leader><Left>", ss.move_cursor_left, "Pane left" },
-			{ "<leader><Right>", ss.move_cursor_right, "Pane right" },
-			{ "<leader><Up>", ss.move_cursor_up, "Pane up" },
-			{ "<leader><Down>", ss.move_cursor_down, "Pane down" },
+			{ "<leader><Left>", ss.move_cursor_left, "Go to left pane" },
+			{ "<leader><Right>", ss.move_cursor_right, "Go to rigth pane" },
+			{ "<leader><Up>", ss.move_cursor_up, "Go to top pane" },
+			{ "<leader><Down>", ss.move_cursor_down, "Go to bottom pane" },
 		}
-
 		for _, m in ipairs(maps) do
 			vim.keymap.set("n", m[1], m[2], { desc = m[3] })
 		end
