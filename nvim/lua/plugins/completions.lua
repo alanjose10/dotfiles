33779becba697d7f9f8083fca068d7b72bffff95
Alan Jose <alanjose10@gmail.com>
2025-12-17 01:44:47 +0530

    using blink for cpm

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,76 +1,50 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
+		"saghen/blink.cmp",
 		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
 			"L3MON4D3/LuaSnip",
-			"hrsh7th/cmp-cmdline",
-			"windwp/nvim-autopairs",
 		},
-		config = function()
-			local cmp = require("cmp")
+		version = "1.*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' (recommend-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappingsed) for mappings similar to built-in completions (C-y to accept)
 
-			-- Snippets + bundled vscode snippets
-			require("luasnip.loaders.from_vscode").lazy_load()
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "default" },
 
-			-- Cmdline completion (paths + commands)
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
+			appearance = {
+				nerd_font_variant = "mono",
+			},
 
-			-- Search completion from current buffer
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = { documentation = { auto_show = false } },
 
-			-- Insert-mode completion UI
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered({
-						border = "rounded",
-					}),
-					documentation = cmp.config.window.bordered({
-						border = "rounded",
-					}),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
 
-			-- Autopairs integration so confirmed items insert matching brackets/quotes
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
+			snippets = { preset = "luasnip" },
+
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
 	},
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -131,15 +131,6 @@ return {
 				end,
 			})
 
-			-- 	-- Popup diagnostic
-			-- 	vim.o.updatetime = 250
-			-- 	vim.diagnostic.config({
-			-- 		virtual_text = true,
-			-- 		signs = true,
-			-- 		underline = true,
-			-- 		update_in_insert = false,
-			-- 		severity_sort = true,
-			-- 	})
 			vim.api.nvim_create_autocmd("CursorHold", {
 				callback = function()
 					vim.diagnostic.open_float(nil, { focus = false })
