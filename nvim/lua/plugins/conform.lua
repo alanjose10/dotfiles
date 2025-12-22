817fb3da63fc7384f9a3a84a7ef92ff9c47016d8
Alan Jose <alanjose10@gmail.com>
2025-12-22 16:01:53 +0530

    adding gofumt formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,43 +1,46 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  "stevearc/conform.nvim", -- modern formatting plugin for Neovim
+  event = { "BufWritePre" }, -- load plugin just before saving (for format-on-save)
+  cmd = { "ConformInfo" }, -- also load when running :ConformInfo command
+
+  -- Keymaps - defined here so they're available immediately (lazy.nvim handles loading)
   keys = {
     {
-      "<leader>cf",
+      "<leader>cf", -- keymap: space + c + f
       function()
-        require("conform").format({ lsp_format = "fallback" })
+        require("conform").format({ lsp_format = "fallback" }) -- format using conform, fall back to LSP if no formatter
       end,
-      mode = { "n", "v" },
-      desc = "Format buffer",
+      mode = { "n", "v" }, -- works in normal and visual mode
+      desc = "Format buffer", -- description shown in which-key
     },
   },
+
   opts = {
-    -- Define formatters by filetype
+    -- Define which formatter to use for each filetype
     formatters_by_ft = {
-      lua = { "stylua" },
+      lua = { "stylua" }, -- use stylua for Lua files
+      go = { "gofumpt" }, -- use gofumpt for Go files (stricter than gofmt)
       -- Add more formatters here as needed:
       -- python = { "black" },
       -- javascript = { "prettier" },
-      -- go = { "gofmt" },
     },
 
-    -- Customize stylua to use spaces instead of tabs
+    -- Customize formatter behavior by passing command-line arguments
     formatters = {
       stylua = {
         prepend_args = {
-          "--indent-type",
+          "--indent-type", -- force stylua to use spaces instead of tabs
           "Spaces",
-          "--indent-width",
+          "--indent-width", -- set indentation width to 2 spaces
           "2",
         },
       },
     },
 
-    -- Format on save configuration
+    -- Automatically format on save
     format_on_save = {
-      timeout_ms = 500, -- timeout for formatting
-      lsp_format = "fallback", -- use LSP formatting as fallback if no formatter configured
+      timeout_ms = 500, -- maximum time to wait for formatting to complete
+      lsp_format = "fallback", -- use LSP formatting if no conform formatter is configured
     },
   },
 }
