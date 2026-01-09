13c12196a4022496797cc7290cabda4ca839498c
Alan <alan@Alans-Mac-mini.local>
2026-01-09 17:17:43 +0000

    fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "master",
-  event = { "BufReadPost", "BufNewFile" },
+  lazy = false,
   build = ":TSUpdate", -- automatically update parsers on install/update
   config = function()
     local config = require("nvim-treesitter.configs")
