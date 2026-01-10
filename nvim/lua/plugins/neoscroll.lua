93782c6657b110d31e3d96a9ab81172d40ff1cba
Alan <alan@Alans-Mac-mini.local>
2026-01-10 08:19:31 +0000

    neoscroll setup

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -1,29 +1,30 @@
 return {
-	"karb94/neoscroll.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("neoscroll").setup({
-			easing_function = "quadratic", -- Controls "smoothness" of the scroll animation
-			hide_cursor = true, -- Hide cursor while scrolling for smoother visuals
-		})
+  "karb94/neoscroll.nvim",
+  event = "VeryLazy",
+  config = function()
+    local ns = require("neoscroll")
 
-		local ns = require("neoscroll")
+    ns.setup({
+      easing_function = "quadratic", -- Controls "smoothness" of the scroll animation
+      hide_cursor = true, -- Hide cursor while scrolling for smoother visuals
+    })
 
-		local mappings = {
-			-- scroll window *up* one line WITHOUT moving cursor
-			["<C-u>"] = function()
-				ns.scroll(-0.1, { move_cursor = false, duration = 100 })
-			end,
+    local mappings = {
+      -- Half-page down, then center cursor
+      ["<C-d>"] = function()
+        ns.scroll(vim.wo.scroll, { move_cursor = true, duration = 300 })
+        vim.cmd("normal! zz")
+      end,
 
-			-- scroll window *down* one line WITHOUT moving cursor
-			["<C-e>"] = function()
-				ns.scroll(0.1, { move_cursor = false, duration = 100 })
-			end,
-		}
+      -- Half-page up, then center cursor
+      ["<C-u>"] = function()
+        ns.scroll(-vim.wo.scroll, { move_cursor = true, duration = 300 })
+        vim.cmd("normal! zz")
+      end,
+    }
 
-		-- APPLY KEYMAPS
-		for key, func in pairs(mappings) do
-			vim.keymap.set("n", key, func, { silent = true, desc = "Smooth scroll: " .. key })
-		end
-	end,
+    for key, func in pairs(mappings) do
+      vim.keymap.set("n", key, func, { silent = true, desc = "Smooth scroll: " .. key })
+    end
+  end,
 }
