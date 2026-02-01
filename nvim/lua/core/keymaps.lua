57ccb326984fff3b1d9b2068cba04ec212d8bde7
Alan Jose <alanjose10@gmail.com>
2026-02-01 19:45:48 +0000

    setup window resize keymaps and remove unnecessary ones

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -10,29 +10,26 @@ vim.keymap.set("n", "<A-Up>", "<C-w>k", { desc = "Move to window above" })
 vim.keymap.set("n", "<A-Right>", "<C-w>l", { desc = "Move to right window" })
 
 -- Resize windows
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+vim.keymap.set("n", "<A-S-Up>", "<cmd>resize +5<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<A-S-Down>", "<cmd>resize -5<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<A-S-Left>", "<cmd>vertical resize -5<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<A-S-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase window width" })
+
+-- Window management
+vim.keymap.set("n", "<leader>wm", "<cmd>wincmd |<cr><cmd>wincmd _<cr>", { desc = "Maximize window" })
+vim.keymap.set("n", "<leader>w=", "<cmd>wincmd =<cr>", { desc = "Equalize windows" })
 
 -- Buffer navigation
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
 
 -- Better indenting (stay in visual mode)
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
 
--- Move lines
-vim.keymap.set("n", "<A-S-Down>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-S-Up>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("v", "<A-S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-S-Left>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
 -- Navigate quick fix list
-vim.keymap.set("n", "<M-S-Right>", ":cnext<CR>", { desc = "Quickfix: Next item" })
-vim.keymap.set("n", "<M-S-Left>", ":cprev<CR>", { desc = "Quickfix: Previous item" })
+vim.keymap.set("n", "<leader><Up>", ":cnext<CR>", { desc = "Quickfix: Next item" })
+vim.keymap.set("n", "<leader><Down>", ":cprev<CR>", { desc = "Quickfix: Previous item" })
 
 vim.keymap.set("n", "<leader>us", function()
 	vim.opt.spell = not (vim.opt.spell:get())
