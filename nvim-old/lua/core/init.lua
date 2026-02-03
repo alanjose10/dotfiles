2bd920b4bcc5e1250d2dfa4ca9b7efe044725d0f
Alan Jose <alanjose10@gmail.com>
2026-02-03 11:30:01 +0000

    [nvim] remove old version

--- nvim-old/lua/core/init.lua
+++
@@ -1,7 +0,0 @@
-require("core.options")
-require("core.keymaps")
-require("core.filetypes")
-require("core.autocmds")
-require("core.tmux")
-
-require("core.popup-menu").setup()
--- nvim-old/lua/plugins/nvim-autopairs.lua
+++
@@ -1,8 +0,0 @@
--- Auto-pairing of brackets, quotes, etc.
--- Properly jumps over closing characters instead of inserting duplicates
-return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = true,
-  opts = {},
-}
