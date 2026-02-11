409d4500fcda0bc106dca7ed492488bcacc094c6
Alan Jose <alanjose10@gmail.com>
2026-02-11 00:37:23 +0000

    enable smart-splits window navigation

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,21 +3,11 @@
 -- Clear search highlight
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
--- Window navigation
-vim.keymap.set("n", "<A-Left>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<A-Down>", "<C-w>j", { desc = "Move to window below" })
-vim.keymap.set("n", "<A-Up>", "<C-w>k", { desc = "Move to window above" })
-vim.keymap.set("n", "<A-Right>", "<C-w>l", { desc = "Move to right window" })
-
--- Resize windows
-vim.keymap.set("n", "<A-S-Up>", "<cmd>resize +5<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<A-S-Down>", "<cmd>resize -5<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<A-S-Left>", "<cmd>vertical resize -5<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<A-S-Right>", "<cmd>vertical resize +5<CR>", { desc = "Increase window width" })
-
 -- Window management
 vim.keymap.set("n", "<leader>wm", "<cmd>wincmd |<cr><cmd>wincmd _<cr>", { desc = "Maximize window" })
 vim.keymap.set("n", "<leader>w=", "<cmd>wincmd =<cr>", { desc = "Equalize windows" })
+vim.keymap.set("n", "<leader>%", "<cmd>vsplit<cr>", { desc = "Vertical spilt" })
+vim.keymap.set("n", '<leader>"', "<cmd>split<cr>", { desc = "Horizontal spilt" })
 
 -- Better indenting (stay in visual mode)
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left" })
@@ -26,9 +16,6 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right" })
 vim.keymap.set("n", "<leader>us", function()
 	vim.opt.spell = not (vim.opt.spell:get())
 end, { desc = "Toggle Spell Check" })
-
-vim.keymap.set("n", "<leader>%", "<cmd>vsplit<cr>", { desc = "Vertical spilt" })
-vim.keymap.set("n", '<leader>"', "<cmd>split<cr>", { desc = "Horizontal spilt" })
 
 -- Paste on a new line (Force Linewise)
 vim.keymap.set("n", "]p", "<cmd>pu<cr>", { desc = "Paste below line" })
---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,65 @@
+return {
+	"mrjones2014/smart-splits.nvim",
+	lazy = false,
+	keys = {
+		-- 1. Moving between splits (Alt + Arrows)
+		{
+			"<M-Left>",
+			function()
+				require("smart-splits").move_cursor_left()
+			end,
+			desc = "Move left",
+		},
+		{
+			"<M-Down>",
+			function()
+				require("smart-splits").move_cursor_down()
+			end,
+			desc = "Move down",
+		},
+		{
+			"<M-Up>",
+			function()
+				require("smart-splits").move_cursor_up()
+			end,
+			desc = "Move up",
+		},
+		{
+			"<M-Right>",
+			function()
+				require("smart-splits").move_cursor_right()
+			end,
+			desc = "Move right",
+		},
+
+		-- 2. Resizing splits (Alt + Shift + Arrows)
+		{
+			"<M-S-Left>",
+			function()
+				require("smart-splits").resize_left()
+			end,
+			desc = "Resize left",
+		},
+		{
+			"<M-S-Down>",
+			function()
+				require("smart-splits").resize_down()
+			end,
+			desc = "Resize down",
+		},
+		{
+			"<M-S-Up>",
+			function()
+				require("smart-splits").resize_up()
+			end,
+			desc = "Resize up",
+		},
+		{
+			"<M-S-Right>",
+			function()
+				require("smart-splits").resize_right()
+			end,
+			desc = "Resize right",
+		},
+	},
+}
