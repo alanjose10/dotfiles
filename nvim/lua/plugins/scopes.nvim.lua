3bed2af085a7a72dd3fb0f405c0970aacdc8b6d8
Alan Jose <alanjose10@gmail.com>
2026-03-02 09:42:23 +0000

    promote scopes to main branch

--- nvim/lua/plugins/scopes.nvim.lua
+++ nvim/lua/plugins/scopes.nvim.lua
@@ -1,11 +1,10 @@
 local dev = false
-local version = "v0.1.0"
 
 return {
 	{
 		"alanjose10/scopes.nvim",
 		dir = dev and "~/repos/scopes.nvim" or nil,
-		tag = dev and nil or version,
+		branch = "main",
 		dependencies = { "folke/snacks.nvim" },
 		config = function()
 			require("scopes").setup({
@@ -15,9 +14,5 @@ return {
 				},
 			})
 		end,
-		-- keys = {
-		-- 	{ "<leader>so", desc = "Scope: open at cursor" },
-		-- 	{ "<leader>sO", desc = "Scope: open at root" },
-		-- },
 	},
 }
